5aff3a7c1094d1c25edae5c25088cf0ff8ece355
zhangchaojie <1098626505@qq.com>
2017-09-25 08:38:04 +0800

    添注释

--- extend/wxpay/Query.php
+++ extend/wxpay/Query.php
@@ -12,6 +12,15 @@ Loader::import('wxpay.lib.WxPayApi');
 *
 * 用法:
 * 调用 \wxpay\Query::exec($query_no) 即可
+*
+*
+* ----------------- 求职 ------------------
+* 姓名: zhangchaojie      邮箱: zhangchaojie_php@qq.com  应届生
+* 期望职位: PHP初级工程师   地点: 深圳(其他城市亦可)
+* 能力:
+*     1.熟悉小程序开发, 前后端皆可
+*     2.后端, PHP基础知识扎实, 熟悉ThinkPHP5框架, 用TP5做过CMS, 商城, API接口
+*     3.MySQL, Linux都在进行进一步学习
 */
 class Query
 {
